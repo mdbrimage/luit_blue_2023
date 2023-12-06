034101ff75a6841a380378e9e7fae736e6e7726f
Michael Brimage <mbrimage58@gmail.com>
2023-12-06 00:49:53 +0000

    Exercise_1 passed the test!

---
+++ exercise_1.py
@@ -0,0 +1,10 @@
+"""
+Let's practice the print() function. Show the following message to the output:
+
+I'm learning to become a Python developer!
+I'm so excited!
+
+Remember that the second sentence must appear on a separate line. Remember that any typo in your solution will make it marked as incorrect.
+"""
+
+print('I\'m learning to become a Python developer!\nI\'m so excited!')
